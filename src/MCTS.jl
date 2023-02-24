8560b958b4978dd3bd65a7abd1f04f186b6c370c
Sam Anklesaria <anklesaria@utexs.edu>
2023-02-24 11:14:29 -0600

    Add back normalization- tests still pass

--- src/MCTS.jl
+++ src/MCTS.jl
@@ -443,18 +443,18 @@ function normalized(st)
   end
   
   # # Flip the board left or right
-  # st2 = fmap(flip_pos_vert, st)
-  # if hash(st2) > hash(st)
-  #   st = st2
-  #   push!(action_map, FlipVert())
-  # end
+  st2 = fmap(flip_pos_vert, st)
+  if hash(st2) > hash(st)
+    st = st2
+    push!(action_map, FlipVert())
+  end
   
   # # Sort tokens of each player
-  # ixs = fmapstructure(a->sortperm(nestedview(a)), st; exclude=ismat)
-  # st = fmap(st, ixs) do a, ix
-  #   ix === (()) ? a : a[:, ix]
-  # end
-  # push!(action_map, TokenPerm(invperm(ixs.positions[1].pieces)))
+  ixs = fmapstructure(a->sortperm(nestedview(a)), st; exclude=ismat)
+  st = fmap(st, ixs) do a, ix
+    ix === (()) ? a : a[:, ix]
+  end
+  push!(action_map, TokenPerm(invperm(ixs.positions[1].pieces)))
   
   assert_valid_state(st)
   Transformation(action_map, value_map), st
