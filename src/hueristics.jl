920695ef58a8a86fb31bc5f0835110a298e81db1
Sam Anklesaria <anklesaria@utexs.edu>
2023-03-10 20:37:43 -0600

    Actually add huersitics

---
+++ src/hueristics.jl
@@ -0,0 +1,21 @@
+function ball_yval_hueristic(st::State)
+  y = st.positions[st.player].ball[2]
+  if st.player == 1
+    y / limits[2]
+  else
+    (limits[2] - y + 1) / limits[2]
+  end
+end
+
+function piece_yval_hueristic(st::State)
+  if st.player == 1
+    y = maximum(st.positions[1].pieces[2,:])
+    return 0.5f0 * (y / limits[2])
+  else
+    y = minimum(st.positions[2].pieces[2,:])
+    return 0.5f0 * (limits[2] - y + 1) / limits[2]
+  end
+end
+
+# the_hueristic(st) = piece_yval_hueristic(st)
+the_hueristic(st) = 0f0
